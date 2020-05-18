d99acdf4fd6c389b2fa2af17ecf7c6b32f70fb62
Taha Bouhsine <45195299+skywolfmo@users.noreply.github.com>
2020-05-18 16:45:43 +0000

    auth is working

--- dist-server/app_server/routes/auth.js
+++ dist-server/app_server/routes/auth.js
@@ -11,18 +11,29 @@ var _User = _interopRequireDefault(require("../../app_api/models/User/User"));
 
 var _auth = require("../controllers/auth");
 
+var _bodyParser = _interopRequireDefault(require("body-parser"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 // import bodyParser from 'body-parser';
 var router = _express["default"].Router(); // router.use(bodyParser.json());
 // router.use(bodyParser.urlencoded({ extended: true }));
-//Auth Routes
 
 
-router.route('/register').post(function (req, res) {
+var jsonParser = _bodyParser["default"].json({
+  limit: '50mb'
+});
+
+var urlParser = _bodyParser["default"].urlencoded({
+  limit: '50mb',
+  extended: true
+}); //Auth Routes
+
+
+router.route('/register').post(jsonParser, urlParser, function (req, res) {
   (0, _auth.register)(req, res); // ctrlAuth.register(req, res);
 });
-router.route('/login').post(function (req, res) {
+router.route('/login').post(jsonParser, urlParser, function (req, res) {
   (0, _auth.login)(req, res); // ctrlAuth.login(req, res);
 }); //create a forgot password
 //end of Auth Routes
--- server/app_server/routes/auth.mjs
+++ server/app_server/routes/auth.mjs
@@ -1,21 +1,26 @@
 import express from 'express';
 import User from '../../app_api/models/User/User';
 import { register, login } from '../controllers/auth';
+import bodyParser from 'body-parser';
+
 // import bodyParser from 'body-parser';
 const router = express.Router();
 // router.use(bodyParser.json());
 // router.use(bodyParser.urlencoded({ extended: true }));
+var jsonParser = bodyParser.json({ limit: '50mb' });
+var urlParser = bodyParser.urlencoded({ limit: '50mb', extended: true });
+
 //Auth Routes
 router
     .route('/register')
-    .post((req, res) => {
+    .post(jsonParser, urlParser, (req, res) => {
         register(req, res);
         // ctrlAuth.register(req, res);
     });
 
 router
     .route('/login')
-    .post((req, res) => {
+    .post(jsonParser, urlParser, (req, res) => {
         login(req, res);
         // ctrlAuth.login(req, res);
     });
